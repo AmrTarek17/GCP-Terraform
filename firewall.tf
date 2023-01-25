777aaf864a08db0c7581b54165b2b69162e86543
AmrTarek17 <amro.tarek6@gmail.com>
2023-01-25 16:31:07 +0000

    removing (deny all egress firwall rule) for restricted subnet

--- firewall.tf
+++ firewall.tf
@@ -16,15 +16,3 @@ resource "google_compute_firewall" "allow_ssh" {
 
 }
 
-resource "google_compute_firewall" "deny_all" {
-  project     = var.project_name
-  name        = "deny-all"
-  network     = var.vpc_name
-  direction = "EGRESS"
-  source_ranges = [ var.sub1_cidr ]
-  
-  deny {
-    protocol  = "all"
-  }
-
-}
